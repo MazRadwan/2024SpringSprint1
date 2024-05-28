b297153b8c01ed6cb17208f5a52d24f738e45530
Maz Radwan <maz.radwan@keyin.com>
2024-05-28 18:16:26 -0230

    basic CLI immplemented

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-global.DEBUD = true;
+global.DEBUG = true;
 
 const fs = require("fs");
+
+const myArgs = process.argv.slice(2);
+
+if (DEBUG) if (myArgs.length >= 1) console.log("myArgs: ", myArgs);
+
+switch (myArgs[0]) {
+  case "init":
+  case "i":
+    if (DEBUG) console.log(myArgs[0], "- init the app");
+    break;
+  case "config":
+  case "c":
+    if (DEBUG) console.log(myArgs[0], "- create the config file and folder");
+    break;
+  case "token":
+  case "t":
+    if (DEBUG) console.log(myArgs[0], "- create the token file");
+    break;
+  case "--help":
+  case " --h":
+  default:
+    if (DEBUG) console.log(myArgs[0], "- display help");
+  // read the help file
+}
