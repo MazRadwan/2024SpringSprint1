64c123c78b26804c08ca2b88ac798de2b41fa0ad
Maz Radwan <maz.radwan@keyin.com>
2024-05-30 10:07:56 -0230

    config and init file

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ global.DEBUG = true;
 
 const fs = require("fs");
 
+const { initializeApplication } = require("./init.js");
+const { configApplication } = require("./config.js");
+const { tokenApplication } = require("./token.js");
+
 const myArgs = process.argv.slice(2);
 
 if (DEBUG) if (myArgs.length >= 1) console.log("myArgs: ", myArgs);
@@ -9,19 +13,25 @@ if (DEBUG) if (myArgs.length >= 1) console.log("myArgs: ", myArgs);
 switch (myArgs[0]) {
   case "init":
   case "i":
-    if (DEBUG) console.log(myArgs[0], "- init the app");
+    if (DEBUG) console.log(myArgs[0], "- init the app.");
+    initializeApplication();
     break;
   case "config":
   case "c":
-    if (DEBUG) console.log(myArgs[0], "- create the config file and folder");
+    if (DEBUG) console.log(myArgs[0], "- create the config folders and files.");
+    configApplication();
     break;
   case "token":
   case "t":
-    if (DEBUG) console.log(myArgs[0], "- create the token file");
+    if (DEBUG) console.log(myArgs[0], "- manage the tokens");
+    tokenApplication();
     break;
   case "--help":
-  case " --h":
+  case "--h":
   default:
     if (DEBUG) console.log(myArgs[0], "- display help");
-  // read the help file
+    fs.readFile(__dirname + "/usage.txt", (error, data) => {
+      if (error) throw error;
+      console.log(data.toString());
+    });
 }
